bc8d9dd2a48563e50f196661edcdad4e377ea91e
hammadhafiz <m.hammad2253815@gamil.com>
2024-11-01 07:46:17 -0700

    first commit

---
+++ java script practice/app.js
@@ -0,0 +1,91 @@
+// alert('thank you');
+
+// var bold ="thanks for purchase"
+// alert (bold)
+
+// var x = 'its a laptop'
+// var s = 'hammad'
+// var d = 'javascript'
+// alert(x+s+d)
+
+
+// var t = '450'
+// var e = 14
+// alert(t+e)
+
+
+// var j='804'
+// var i='408'
+// alert(j+i)
+
+
+// var h =prompt( "which country is this")
+// var a = "right"
+
+// if(h=== "pakistan"){
+//     alert(a)
+// }
+
+// else(alert("wrong"))
+
+// var num = 804;
+// var name = 150
+// var good = 804+150
+// alert(good)
+
+// var s = 549
+// var h = 379
+// var plus = s-h
+// alert(plus)
+
+
+// var j = 899
+// var t = 744
+// var f = j*t
+// alert(f)
+
+
+// var b = 209
+// var w = 367
+// var c = 209 / 367
+// alert(c)
+
+
+// var i = 675
+// var n = 856
+// var m = i%n
+// alert(m)
+
+
+// var ham = 25 
+// var mad = ham++
+// alert(ham)
+
+
+// var you = 786
+// var me = you++
+// alert(you)
+
+
+
+// var p = prompt("where are you from")
+// if( p=== "pakistan"){
+//     alert("Correct")
+// }
+// else{
+//     alert("Wrong")
+// }
+
+
+// var x = prompt("where are you from")
+// if (x==="india"){
+//     alert("correct")
+// }
+// else{
+//     alert("wrong")
+// }
+
+
+
+
+
